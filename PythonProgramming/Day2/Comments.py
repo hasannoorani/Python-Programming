6933d7eb8cd2d30e4e1e0427332a69717040954f
hasannoorani <hasan.business92@gmail.com>
2024-12-12 23:09:20 +0530

    first commit

---
+++ PythonProgramming/Day2/Comments.py
@@ -0,0 +1,21 @@
+#  --> this is comment
+
+#Single line comment --
+#Hello world 
+#Which programming language your are choose
+#Python
+
+
+#Multiline commnets --
+''' 
+Hello world 
+Which programming language your are choose
+Python
+'''
+#OR
+
+"""
+Hello world 
+Which programming language your are choose
+Python
+"""
